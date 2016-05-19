0e3ec9a20bccf23c19e54070e4d5e9cbee4c3ddb
igorolivei <igorolivei@github.com>
2016-05-19 12:05:53 -0300

    Subindo plugin

---
+++ estrutura/desinstalacao.sql
@@ -0,0 +1,7 @@
+DROP TABLE plugins.tomadorsuprimento;
+
+DROP SEQUENCE plugins.tomadorsuprimento_sequencial_seq;
+
+DROP TABLE plugins.empauttomador;
+
+DROP SEQUENCE plugins.empauttomador_sequencial_seq;
---
+++ estrutura/instalacao.sql
@@ -0,0 +1,25 @@
+CREATE TABLE plugins.tomadorsuprimento (
+	sequencial integer,
+	orgao integer,
+	unidade integer,
+	numcgm integer,
+	ativo boolean);
+
+CREATE SEQUENCE plugins.tomadorsuprimento_sequencial_seq
+INCREMENT 1
+MINVALUE 1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+CREATE TABLE plugins.empauttomador (
+	sequencial integer, 
+	autorizacaoempenho integer, 
+	tomadorsuprimento integer);
+
+CREATE SEQUENCE plugins.empauttomador_sequencial_seq
+INCREMENT 1
+MINVALUE 1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
