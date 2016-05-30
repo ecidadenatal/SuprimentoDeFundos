ae37804e972850a0c5267b03663d2742fb8508bb
igorolivei <igorolivei@github.com>
2016-05-30 16:11:23 -0300

    Alteração nas expressões regulares de busca para alterações no relatório

--- estrutura/desinstalacao.sql
+++ estrutura/desinstalacao.sql
@@ -5,3 +5,7 @@ DROP SEQUENCE plugins.tomadorsuprimento_sequencial_seq;
 DROP TABLE plugins.empauttomador;
 
 DROP SEQUENCE plugins.empauttomador_sequencial_seq;
+
+DROP TABLE plugins.empprestaaprovacao;
+
+DROP SEQUENCE plugins.empprestaaprovacao_sequencial_seq;
--- estrutura/instalacao.sql
+++ estrutura/instalacao.sql
@@ -23,3 +23,16 @@ MINVALUE 1
 MAXVALUE 9223372036854775807
 START 1
 CACHE 1;
+
+CREATE TABLE plugins.empprestaaprovacao (
+	sequencial integer,
+	emppresta integer,
+	aprovado boolean
+);
+
+CREATE SEQUENCE plugins.empprestaaprovacao_sequencial_seq
+INCREMENT 1
+MINVALUE 1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
